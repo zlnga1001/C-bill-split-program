b400f897e1a07e328440dbfab71a02d4cb5bb515
Nga Vu <134727438+zlnga1001@users.noreply.github.com>
2025-01-28 22:34:07 -0500

    finish program structure section

--- funtion_template/sum_program.cpp
+++ funtion_template/sum_program.cpp
@@ -3,6 +3,19 @@
 #include <iostream>
 using namespace std;
 
+namespace foo{
+    int value(){
+        return 5;
+    }
+}
+
+namespace bar{
+    const double pi = 3.1416;
+    double value() {
+        return 2 * pi;
+    }
+}
+
 template <class T>
 T sum(T a, T b){
     T result;
@@ -10,11 +23,16 @@ T sum(T a, T b){
     return result;
 }
 
-int main () {
-    int i = 5, j = 6, k;
+int x; // static storage -  global variables
+
+int main()
+{
+    int i = 5, j = 6, k; //automatic storage - local variables
     double e = 2.0, f = 1.2, g;
     k = sum<int>(i, j);
     g = sum<double>(e, f);
     cout << "This is " << k << "\n";
     cout << "This is " << g << "\n";
+    cout << foo::value() << "\n";
+    cout << bar::value() << "\n";
 }
