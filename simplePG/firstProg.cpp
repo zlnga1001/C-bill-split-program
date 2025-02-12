9b79c651252adccfac999c4f3e28a5ae95c1dacb
Nga Vu <134727438+zlnga1001@users.noreply.github.com>
2025-02-11 23:23:54 -0500

    modify to improve visuality

--- simplePG/firstProg.cpp
+++ simplePG/firstProg.cpp
@@ -7,6 +7,13 @@ int main()
     std::cout << "Enter an integer: ";
     std::cin >> integer;
     std::cout << "You entered " << integer << "\n";
+    //std::cout << "Double that number is " << integer*2 << "\n"; //this is not a really good approach because it's gonan change uer's input
+
+    int doublenum = integer * 2; 
+    std::cout << "Double that number is " << doublenum << "\n";
+    std::cout << "User input number is " << integer << "\n";
+
+    return 0;
 }
 
 
