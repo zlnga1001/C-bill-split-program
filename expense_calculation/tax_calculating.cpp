23f2c6e1db4e9feed2ece16c9d2a885c83b1c400
Nga Vu <134727438+zlnga1001@users.noreply.github.com>
2025-02-23 13:07:42 -0500

    finished, needed more edge case on calculating negative bill and also should fix user's entry on how many dish one's order

--- expense_calculation/tax_calculating.cpp
+++ expense_calculation/tax_calculating.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <map>
+#include <string>
 
-// void foo()
-// {
-//     main(); // Compile error: main not allowed to be called explicitly
-// }
+/*
+Objective: Calculate the tax and tip of a bill when hanging out with friends
+There are 2 cases:
+1. Split evenly
+2. Spit by individual
 
-// void main() // Compile error: main not allowed to have non-int return type
-// {
-//     foo();
-// }
+*/
+
 
 double getDoubleValueFromUser(){
     double intput_amount;
-    std::cout << "Enter the amount\n";
+
+    std::cout << " Enter the amount in double\n";
     std::cin >> intput_amount;
 
     return intput_amount;
@@ -20,41 +22,104 @@ double getDoubleValueFromUser(){
 
 double getIntValueFromUser(){
     int input;
-    std::cout << "Enter the amount\n";
+    std::cout << " Enter the amount in integer\n";
     std::cin >> input;
 
     return input;
 }
 
-void casesForBillSplitting(){
-    std::cout << "What options best describe this bill splitting? Enter the number of choice";
-    std::cout << "1. Split evenly";
-    std::cout << "2. Spit by bill";
-
-    int option;
-    std::cin >> option;
-    switch (option) {
-        case 1: 
-        case 2:
-        
-    }
-}
 
 int main(){
 
-
-    double total_bill;
-    int total_people;
-
-    std::cout << "What is your total bill amount? \n";
-    total_bill = getDoubleValueFromUser();
-
-
-    std::cout << "How many people split the bill? \n";
-    total_people = getIntValueFromUser();
-
+    //declare variables for the program hehehe
+    int option;
+    double bill;
+    double tax;
+    double tip;
+    double receipt_total_bill;
+    double individual_bill;
+    int people;
     
 
 
+            
+            
+    std::cout << "How many people join?";
+    people = getIntValueFromUser();
 
-}
+
+    std::cout << "What is your total bill amount without tax and tip (in dollars)?";
+    bill = getDoubleValueFromUser();
+
+            
+    std::cout << "How much tax does this bill take in percentage?";
+    tax = getDoubleValueFromUser();
+            
+     //this case did not count for what if the tip is not in percentage
+    std::cout << "How much tip did we choose to pay in percentage?";
+    tip = getIntValueFromUser();
+
+
+    receipt_total_bill = bill + (tax*bill)/100 + (tip*bill)/100 ;
+    std::cout << "The total bill is " << receipt_total_bill << " dollars\n";
+
+    std::cout << "What options best describe this bill splitting? Enter your choice (number only)\n";
+    std::cout << "1. Split evenly\n";
+    std::cout << "2. Spit by individual\n";
+
+    std::cin >> option;
+    switch (option) {
+        case 1:
+        //splt the bill evenly => asking for total bill, tax, tip, and number of people
+
+            individual_bill = receipt_total_bill/people;
+
+            std::cout << "Each person should pay " << individual_bill << " dollars";
+            break; 
+
+        case 2:
+            //split by individual => asking for total bill, tax, tip, and number of people
+            //asking for each person's bill, using for loop to ask for each person's bill
+            //I will use a hashmap, storing each person name with their bill
+
+
+            //declare variables for calculating each person's bill
+            double total_bill = 0;
+            std::map<std::string, double> person_bill;
+            std::string name;
+            
+
+            for(int i = 0; i < people; i++){
+                std::cout << i+1 << ". Name: " << std::endl;
+                std::cin >> name;
+                std::cout << "Bill (in dollars): " << std::endl;
+                std::cin >> individual_bill;
+                //hashmap map name with individual bill
+                person_bill[name] = individual_bill;
+                total_bill += individual_bill; //khi ma co total bill roi thi compare voi cai total bill trong recipt co
+            }
+
+            if(total_bill != receipt_total_bill){
+                std::cout << "The total bill in the receipt is not equal to the total bill we calculated\n";
+                //thằng nào đó sinh nhật hay gì đó được bao so the number of people who pay the bill will be different and there should be extra added on to each bill
+                std::cout << "Split the rest of the bill by individual \n";
+                total_bill = receipt_total_bill - total_bill;
+                std::cout << "The rest of the bill is " << total_bill << " dollars\n";
+                std::cout << "Split the rest of the bill by individual \n";
+                double individual_bill_after_split = total_bill/people;
+                
+                //recalculate each person's bill
+                for(auto &pair : person_bill){
+                    pair.second += individual_bill_after_split;
+                    std::cout << pair.first << " should now pay " << pair.second << " dollars\n";
+                }
+
+            }
+                            //calculate including tax and tip
+            for(auto &pair : person_bill){
+                pair.second += tax*pair.second + tip*pair.second;
+            }
+            break;
+        }
+    }
+
